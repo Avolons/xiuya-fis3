a8000dffb3e3c341b03e03500b3a8c6a01d25433
wanzhuhao <wanzhuhao@zjzx.cn>
2016-09-17 18:03:13 +0800

    9.17

---
+++ fis3-base/dist/static/app/masterinformation.js
@@ -0,0 +1,39 @@
+new Vue({
+	el:"body",
+	data:function  () {
+		return {
+			cerlist:[
+			{
+				name:"黄娇娇",
+				idcode:130683199208117325,
+				cer:"职业技能证书",
+				way:"2016-02-06",
+				type:"手动"
+			},
+			{
+				name:"黄娇娇",
+				idcode:130683199208117325,
+				cer:"职业技能证书",
+				way:"2016-02-06",
+				type:"手动"
+			},
+			{
+				name:"黄娇娇",
+				idcode:130683199208117325,
+				cer:"职业技能证书",
+				way:"2016-02-06",
+				type:"手动"
+			},
+			]
+		}
+	},
+	ready:function  () {
+		 $(window).scroll(function  () {
+		 	if ($(window).scrollTop()>95) {
+		 		$('.nav-mes').addClass('fix-style');
+		 	}else{
+		 		$('.nav-mes').removeClass('fix-style');
+		 	}
+		 })
+	}
+})
---
+++ fis3-base/modules/app/query.js
@@ -0,0 +1,39 @@
+new Vue({
+	el:"body",
+	data:function  () {
+		return {
+			cerlist:[
+			{
+				name:"黄娇娇",
+				idcode:130683199208117325,
+				cer:"职业技能证书",
+				way:"2016-02-06",
+				type:"手动"
+			},
+			{
+				name:"黄娇娇",
+				idcode:130683199208117325,
+				cer:"职业技能证书",
+				way:"2016-02-06",
+				type:"手动"
+			},
+			{
+				name:"黄娇娇",
+				idcode:130683199208117325,
+				cer:"职业技能证书",
+				way:"2016-02-06",
+				type:"手动"
+			},
+			]
+		}
+	},
+	ready:function  () {
+		laypage({
+				cont: $('#pagecontrol'), //容器。值支持id名、原生dom对象，jquery对象,
+			    pages: 100, //总页数
+			    skip: true, //是否开启跳页
+			    skin: '#AF0000',
+			    groups: 3 //连续显示分页数
+			});
+	}
+})
