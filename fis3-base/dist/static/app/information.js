1d9af4d1c5af53f251f75d0b830a74c72c70daa5
wanzhuhao <wanzhuhao@zjzx.cn>
2016-09-21 18:14:11 +0800

    9.21

---
+++ fis3-base/dist/static/app/information.js
@@ -0,0 +1,26 @@
+new Vue({
+	el:'body',
+	data:function  () {
+		return{
+			//table切换
+			table:{
+				itr:{show:true},
+				list:{show:false},
+				talk:{show:false}
+			}
+		}
+	},
+	//方法区
+	methods:{
+		tablecheck:function  (item) {
+			if (item.show) {
+				return false;
+			}else{
+				this.table.itr.show=false;
+				this.table.list.show=false;
+				this.table.talk.show=false;
+				item.show=true;
+			}
+		}
+	}
+})
